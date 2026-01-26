938a41dc5d29c7de8d1000b618a9b155544ee5f6
becoentertainment <hello@getnabi.app>
2026-01-26 18:47:11 +0100

    Aggiunto favicon/icona Nabi a tutte le pagine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Nabi - La tua app di journaling',
   description: 'Scopri Nabi, l\'app per il journaling creativo e personale',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
